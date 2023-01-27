273b9b4e15bfbc1d5975883fb36cdebc09b28273
Thounda Craig <thoundac94@gmail.com>
2023-01-27 17:12:17 -0500

    JavaScript reverse array order

--- 02_02/reverse.js
+++ 02_02/reverse.js
@@ -1,6 +1,10 @@
-const condiments = ['ketchup', 'mustard', 'relish', 'mayo'];
+const condiments = ["ketchup", "mustard", "relish", "mayo"];
 // Use reverse to change the order of this array.
+console.log(condiments.reverse());
 
-const employees = ['Adams', 'Carter', 'Davis', 'Fraklin', 'Zappa'];
+const employees = ["Adams", "Carter", "Davis", "Fraklin", "Zappa"];
 // Use reverse to change the order of this array,
 // but keep the original array intact.
+const reverse = [...employees];
+console.log(`Reversed: `, reverse.reverse());
+console.log(`Original`, employees);
