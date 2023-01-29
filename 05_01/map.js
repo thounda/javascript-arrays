9f26096af09293f3f29dca19696df45153efb4d4
Thounda Craig <thoundac94@gmail.com>
2023-01-28 20:42:41 -0500

    JavaScript map method on arrays

--- 05_01/map.js
+++ 05_01/map.js
@@ -1,7 +1,21 @@
 const friends = [
-  { firstname: 'Jane', lastname: 'Doe' },
-  { firstname: 'John', lastname: 'Smith' },
-  { firstname: 'Alex', lastname: 'Trebek' },
+  { firstname: "Jane", lastname: "Doe" },
+  { firstname: "John", lastname: "Smith" },
+  { firstname: "Alex", lastname: "Trebek" },
 ];
 
-//Create a new array that includes only the full name (firstname + lastname) of each friend.
+//Create a new array that includes only the full name (firstname + lastname) of each friend - IMPLICITLY.
+/*
+const fullName = friends.map((friend) => {
+  return friend.firstname + " " + friend.lastname;
+});
+
+console.log(`Fullname using map() - Implicitly`, fullName);
+*/
+
+// Same program define - EXPLICITLY
+const fullName = friends.map(
+  (friend) => `${friend.firstname} ${friend.lastname}`
+);
+
+console.log(`Fullname using map() - Explicited`, fullName);
