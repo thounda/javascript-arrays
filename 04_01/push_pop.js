7af756fe096772f2b90e20c3b05872277d673bc9
Thounda Craig <thoundac94@gmail.com>
2023-01-28 20:00:09 -0500

    JavaScript array methods PUSH & POP examples

--- 04_01/push_pop.js
+++ 04_01/push_pop.js
@@ -1,6 +1,12 @@
-const drinks = ['coffee', 'tea', 'soda', 'water'];
+const drinks = ["coffee", "tea", "soda", "water"];
 //How do we add "wine" to the array of drinks?
 
-const seasons = ['spring', 'summer', 'fall', 'winter'];
+const seasons = ["spring", "summer", "fall", "winter"];
 //How do we remove the last season from the seasons array?
+drinks.push("wine");
+console.log(drinks);
+
 //Store the element that is removed in a variable.
+const poppedValue = seasons.pop();
+console.log(poppedValue);
+console.log(seasons);
