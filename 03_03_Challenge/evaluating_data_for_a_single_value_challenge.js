34f6c245248676effbad13bb6808886b35271706
Thounda Craig <thoundac94@gmail.com>
2023-01-28 19:53:13 -0500

    JavaScript includes(), some(), and check Even Number examples

--- 03_03_Challenge/evaluating_data_for_a_single_value_challenge.js
+++ 03_03_Challenge/evaluating_data_for_a_single_value_challenge.js
@@ -3,9 +3,16 @@ const bowlingScores = [154, 204, 300, 184, 286];
 
 //Challenge Question #1
 //Does the array of bowling scores include a 300?
+//const includesScore = bowlingScores.some((score) => score == 300);
+const includesScore = bowlingScores.includes(300);
+console.log(`A score equal to 300? ${includesScore}`);
 
 //Challenge Question #2
 //Are some of the scores under 150?
+const someScores = bowlingScores.some((score) => score < 150);
+console.log(`A score under 150? ${someScores}`);
 
 //Challenge Question #3
 //Is every score an even number?
+const everyScore = bowlingScores.every((score) => score % 2 === 0);
+console.log(`Is every score an even #? ${everyScore}`);
