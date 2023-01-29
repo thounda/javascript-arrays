2e516773a851af04170ed6707f68a76d6d944856
Thounda Craig <thoundac94@gmail.com>
2023-01-28 21:22:36 -0500

    JavaScript reducer function

--- 05_03/reduce.js
+++ 05_03/reduce.js
@@ -1,3 +1,8 @@
 const bills = [1200, 29.99, 65.93, 17.32, 44.76];
 
 //Determine how much the total bills are using the reduce method
+const totalBills = bills.reduce((total, bill) => {
+  return total + bill;
+});
+
+console.log(`Explicitly Called`, totalBills);
