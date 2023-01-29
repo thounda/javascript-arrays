964666b889830e741a7f4a8f8c3e91198d69463d
Thounda Craig <thoundac94@gmail.com>
2023-01-28 19:32:47 -0500

    JavaScript some() and every() method examples

--- 03_02/every_some.js
+++ 03_02/every_some.js
@@ -1,14 +1,18 @@
 const states = [
-  'California',
-  'New York',
-  'New Jersey',
-  'Alaska',
-  'New Mexico',
-  'Montana',
+  "California",
+  "New York",
+  "New Jersey",
+  "Alaska",
+  "New Mexico",
+  "Montana",
 ];
 
 //Use this handy string method to find the solution: startsWith("New")
 
 //Do some of the states in the array start with "New"?
+const someStates = states.some((states) => states.startsWith("New"));
+console.log(someStates);
 
 //Does every state in the array start with "New"?
+const everyState = states.every((states) => states.startsWith("New"));
+console.log(everyState);
